3aa0f7a4bad9c5d6a2e4b53b301bae1bc0cde0e3
Octave Larose <octave.larose@hotmail.fr>
2024-07-04 15:45:31 +0100

    unsafe accesses in specialized nodes also

--- som-interpreter-ast/src/specialized/if_node.rs
+++ som-interpreter-ast/src/specialized/if_node.rs
@@ -11,22 +11,20 @@ pub struct IfNode {
 
 impl Invoke for IfNode {
     fn invoke(&self, universe: &mut UniverseAST, args: Vec<Value>) -> Return {
-        let cond_block_val = args.get(0).unwrap();
-        let body_block_arg = args.get(1).unwrap();
+        let cond_block_val = unsafe { args.get_unchecked(0) };
+        let body_block_arg = unsafe { args.get_unchecked(1) };
 
         let (bool_val, body_block) = match (cond_block_val, body_block_arg) {
             (Value::Boolean(b), Value::Block(c)) => (*b, Rc::clone(&c)),
             (a, b) => panic!("if[True|False] was not given a bool and a block as arguments, but {:?} and {:?}", a, b)
         };
-
-        let nbr_locals = body_block.block.nbr_locals;
-
+        
         if bool_val != self.expected_bool {
             Return::Local(Nil)
         } else {
             universe.with_frame(
                 Value::Block(Rc::clone(&body_block)),
-                nbr_locals,
+                body_block.block.nbr_locals,
                 0,
                 |universe| body_block.invoke(universe, vec![]),
             )
