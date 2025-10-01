a734c748652adcc662086bdd175c8e822b274ff4
Octave Larose <octave.larose@hotmail.fr>
2025-10-01 11:02:45 +0200

    mem usage, post havlak fix

--- som-interpreter-ast/src/primitives/system.rs
+++ som-interpreter-ast/src/primitives/system.rs
@@ -195,22 +195,22 @@ fn gc_stats(universe: &mut Universe, stack: &mut GlobalValueStack) -> Result<Vec
         //let total_bytes_bigint = gc_interface.alloc(BigInt::from(gc_interface.get_used_bytes()), AllocSiteMarker::BigInt);
 
         // -----  PROGRAM REPR SIZE -----
-        let total_gc: usize = 24;
-        let total_gc_time = gc_interface.alloc(BigInt::from(-2424), AllocSiteMarker::BigInt);
-        let total_bytes_bigint = gc_interface.alloc(BigInt::from(gc_interface.total_program_repr_size), AllocSiteMarker::BigInt);
+        //let total_gc: usize = 24;
+        //let total_gc_time = gc_interface.alloc(BigInt::from(-2424), AllocSiteMarker::BigInt);
+        //let total_bytes_bigint = gc_interface.alloc(BigInt::from(gc_interface.total_program_repr_size), AllocSiteMarker::BigInt);
 
         // -----  TOTAL MEM USAGE -----
-        //let total_gc: usize = 42;
-        //let total_gc_time = gc_interface.alloc(BigInt::from(-4242), AllocSiteMarker::BigInt);
-        //let total_bytes_bigint = gc_interface.alloc(
-        //    BigInt::from(
-        //        gc_interface.total_program_repr_size
-        //        + gc_interface.total_other_memory_size
-        //        // not fully accurate since the stack may grow later still? but i think it's very much fine
-        //        + (stack.get_capacity() * size_of::<Value>()) as u128,
-        //    ),
-        //    AllocSiteMarker::BigInt,
-        //);
+        let total_gc: usize = 42;
+        let total_gc_time = gc_interface.alloc(BigInt::from(-4242), AllocSiteMarker::BigInt);
+        let total_bytes_bigint = gc_interface.alloc(
+            BigInt::from(
+                gc_interface.total_program_repr_size
+                + gc_interface.total_other_memory_size
+                // not fully accurate since the stack may grow later still? but i think it's very much fine
+                + (stack.get_capacity() * size_of::<Value>()) as u128,
+            ),
+            AllocSiteMarker::BigInt,
+        );
 
         (total_gc, total_gc_time, total_bytes_bigint)
     };
